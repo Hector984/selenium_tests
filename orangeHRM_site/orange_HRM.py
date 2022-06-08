5bfeae377d963299b48d660863e7aaa3b2ad83ad
Hector984 <hectoruam96@gmail.com>
2022-06-07 21:15:39 -0500

    Login test in orangeHRM

--- orangeHRM_site/orange_HRM.py
+++ orangeHRM_site/orange_HRM.py
@@ -14,8 +14,29 @@ class HomeTest(unittest.TestCase):
         driver.maximize_window()
         
     
-    
-    
+    def test_user_login(self):
+        driver = self.driver
+        user_input = driver.find_element(By.ID, 'txtUsername')
+        
+        enable_status = user_input.is_enabled()
+        displayed_status = user_input.is_displayed()
+        
+        print(f'Enable {enable_status} and Displayed {displayed_status}')
+        
+        user_input.clear()
+        attr_data = user_input.get_attribute('type')
+        font_value = user_input.value_of_css_property('font-size')
+        
+        print(f'Attribute of the element {attr_data} and Property {font_value}')
+        
+        user_input.send_keys('Admin')
+        
+        password_input = driver.find_element(By.ID, 'txtPassword')
+        
+        password_input.clear()
+        password_input.send_keys('admin123')
+        
+        btn_login = driver.find_element(By.ID, 'btnLogin').click()
     
     
     def tearDown(self):
